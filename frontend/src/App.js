7f9799a8b2261a7e994541168a52b485194314bd
FlexonaFFt <flexonafft@bk.ru>
2024-07-18 22:49:00 +0300

    little updates

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,8 +2,9 @@ import "./App.css";
 
 function App() {
   return (
-    <section className="body">
+    <section className="main-containers">
       <div className="container left-container"></div>
+      <div className="container right-container"></div>
     </section>
   );
 }
